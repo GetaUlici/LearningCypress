76cb745199dcfd22d000efbf54cdddf1068ca50f
geta.ulici <ulicigeta@gmail.com>
2025-06-19 14:30:19 +0300

    Adding more tests

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -260,8 +260,39 @@ it('Web tables', () => {
         
     })
     })
-    
+})
 
+it('Tooltips', () => {
+    cy.visit('/')
+    cy.contains('Modal & Overlays').click()
+    cy.contains('Tooltip').click()
+
+    cy.contains('nb-card','Colored Tooltips')
+    .contains('Default').click()
+    cy.get('nb-tooltip').should('contain', 'This is a tooltip')
+})
+
+it.only('Dialiog box test', () => {
+    cy.visit('/')
+    cy.contains('Tables & Data').click()
+    cy.contains('Smart Table').click()
+
+    //1. - This is not the best option, but it can be done like this too
+    // cy.get('tbody tr').first().find('.nb-trash').click()
+    // cy.on('window:confirm', (confirm) => {
+    //     expect(confirm).to.equal('Are you sure you want to delete?') 
+    // })
+
+    //2. This is a better approach
+    // const stub = cy.stub()
+    // cy.on('window:confirm', stub)
+    // cy.get('tbody tr').first().find('.nb-trash').click().then (() => {
+    //     expect(stub.getCall(0)).to.be.calledWith('Are you sure you want to delete?')
+    // })
+    
+    //3. - Select Cancel on the dialog box
+    cy.get('tbody tr').first().find('.nb-trash').click()
+    cy.on('window:confirm', () => false)
 })
 
 })
